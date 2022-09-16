b98defb4199f619d52f5d1b86bf7d27f6f286f03
Andres Garcia <andres.fgp@hotmail.com>
2022-09-15 19:14:40 -0500

    TypoOrm UPDATE

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -42,6 +48,12 @@ export class Product {
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) this.slug = this.title;
+    this.slug = this.slug.toLowerCase().replace(/ /g, '_').replace(/'/g, '');
+  }
+
+  // Before Update
+  @BeforeUpdate()
+  checkSlugUpdate() {
     this.slug = this.slug.toLowerCase().replace(/ /g, '_').replace(/'/g, '');
   }
 }
